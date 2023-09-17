529337dc60e91e9a98c8e40e3785a5171a41c24c
Polashahmad01 <dev.polashahmad@gmail.com>
2023-09-18 00:18:15 +0600

    Update styles and text sizes in tip calculator form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export const App = () => {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen">
-      <h1 className="text-white text-5xl font-semibold mb-8">Tip Calculator</h1>
+      <h1 className="text-white text-2xl md:text-5xl font-semibold mb-4 md:mb-8">Tip Calculator</h1>
       <CalculateForm />
     </div>
   )
--- src/components/CalculateForm.jsx
+++ src/components/CalculateForm.jsx
@@ -1,46 +1,46 @@
 export const CalculateForm = () => {
 
   return (
-    <form className="bg-white p-8 flex gap-8 justify-center rounded-2xl w-2/4">
+    <form className="bg-white p-6 flex flex-col gap-4 justify-center rounded-2xl w-3/4 md:w-2/4 md:p-12 md:flex-row md:gap-8">
       <div className="flex-1">
-        <div className="flex flex-col mb-4">
-          <label htmlFor="bill" className="text-xl mb-2">Bill</label>
-          <input type="text" className="border-2 border-[#042f2e] py-[0.55rem] px-[1.15rem] rounded-lg" />
+        <div className="flex flex-col mb-2 md:mb-4">
+          <label htmlFor="bill" className="mb-1 text-lg md:text-xl md:mb-2">Bill</label>
+          <input type="text" className="border-2 rounded-lg border-[#042f2e] py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]" />
         </div>
-        <div className="mb-4">
-          <label htmlFor="select" className="text-xl">Select Tip %</label>
+        <div className="mb-2 md:mb-4">
+          <label htmlFor="select" className="text-lg md:text-xl">Select Tip %</label>
           <div className="grid grid-cols-3 gap-2 mt-2">
-            <button>5%</button>
-            <button>10%</button>
-            <button>15%</button>
-            <button>25%</button>
-            <button>50%</button>
+            <button className="py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]">5%</button>
+            <button className="py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]">10%</button>
+            <button className="py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]">15%</button>
+            <button className="py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]">25%</button>
+            <button className="py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]">50%</button>
             <input type="number" placeholder="Custom" className="border-2 rounded-lg border-[#042f2e] text-center" />
           </div>
         </div>
         <div className="flex flex-col">
-          <label htmlFor="numberOfPeople" className="text-xl mb-2">Number Of People</label>
-          <input type="text" className="border-2 border-[#042f2e] py-[0.55rem] px-[1.15rem] rounded-lg" />
+          <label htmlFor="numberOfPeople" className="text-lg mb-1 md:text-xl md:mb-2">Number Of People</label>
+          <input type="text" className="border-2 rounded-lg border-[#042f2e] py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem]" />
         </div>
       </div>
       <div className="bg-[#042f2e] rounded-2xl p-8 flex flex-col justify-between flex-1 text-white">
         <div>
           <div className="flex justify-between items-center mb-4">
             <div>
-              <h5 className="mb-1 font-extrabold text-lg">Tip Amount</h5>
+              <h5 className="mb-1 font-extrabold text-base md:text-lg">Tip Amount</h5>
               <p className="opacity-50">/ person</p>
             </div>
-            <p className="font-extrabold text-3xl">$0.00</p>
+            <p className="font-extrabold text-xl md:text-3xl">$0.00</p>
           </div>
           <div className="flex justify-between items-center mb-4">
             <div>
               <h5 className="mb-1 font-extrabold text-lg">Total</h5>
               <p className="opacity-50">/ person</p>
             </div>
-            <p className="font-extrabold text-3xl">$0.00</p>
+            <p className="font-extrabold text-xl md:text-3xl">$0.00</p>
           </div>
         </div>
-        <button className="text-white w-full border-[#646cff] tracking-wider transition hover:bg-neutral-700">RESET</button>
+        <button className="text-white w-full border-[#646cff] tracking-wider transition py-[0.40rem] px-[0.90rem] md:py-[0.55rem] md:px-[1.15rem] hover:bg-neutral-700">RESET</button>
       </div>
     </form>
   )
