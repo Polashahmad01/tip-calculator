bcae48a75da95427563ce4082e6030a5938466e2
Polashahmad01 <dev.polashahmad@gmail.com>
2023-09-17 21:30:27 +0600

    Update form layout and styling

--- src/components/CalculateForm.jsx
+++ src/components/CalculateForm.jsx
@@ -1,8 +1,31 @@
 export const CalculateForm = () => {
 
   return (
-    <div>
-      Form
-    </div>
+    <form className="bg-white p-8 rounded-2xl flex justify-between items-center gap-4 w-2/4">
+      <div>
+        <div className="flex flex-col mb-4">
+          <label htmlFor="bill" className="text-xl mb-2">Bill</label>
+          <input type="text" className="border-y border-x border-[#333] py-[0.55rem] px-[1.15rem] rounded-lg" />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="select" className="text-xl">Select Tip %</label>
+          <div className="grid grid-cols-3 gap-2 mt-2">
+            <button>5%</button>
+            <button>10%</button>
+            <button>15%</button>
+            <button>25%</button>
+            <button>50%</button>
+            <input type="text" placeholder="Custom" className="border-y border-x rounded-lg border-[#333] text-center" />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="numberOfPeople" className="text-xl mb-2">Number Of People</label>
+          <input type="text" className="border-y border-x border-[#333] py-[0.55rem] px-[1.15rem] rounded-lg" />
+        </div>
+      </div>
+      <div>
+        <h1 className="text-2xl">Results</h1>
+      </div>
+    </form>
   )
 }
